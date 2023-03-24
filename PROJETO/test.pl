a2f61a961295199fe6edef26d0607e2ed50d14f4
Rodrigo Freire <109775603+rodrigoFfreire@users.noreply.github.com>
2023-03-24 01:34:52 +0000

    Delete test.pl

--- PROJETO/test.pl
+++
@@ -1,6 +0,0 @@
-(X1 = Name1, (X3 = Name2; X6 = Name2; X7 = Name2; X8 = Name2)); (X1 = Name2, (X3 = Name1; X6 = Name1; X7 = Name1; X8 = Name1));
-    (X3 = Name1, (X1 = Name2; X6 = Name2; X7 = Name2; X8 = Name2)); (X3 = Name2, (X1 = Name1; X6 = Name1; X7 = Name1; X8 = Name1));
-    (X6 = Name1, (X1 = Name2; X2 = Name2; X3 = Name2; X8 = Name2)); (X6 = Name2, (X1 = Name1; X2 = Name1; X3 = Name1; X8 = Name1));
-    (X8 = Name1, (X1 = Name2; X2 = Name2; X3 = Name2; X6 = Name2)); (X8 = Name2, (X1 = Name1; X2 = Name1; X7 = Name1; X6 = Name1));
-    (X2 = Name1, (X6 = Name2; X7 = Name2; X8 = Name2)); (X2 = Name2, (X6 = Name1; X7 = Name1; X8 = Name1));
-    (X7 = Name1, (X1 = Name2; X2 = Name2; X3 = Name2)); (X7 = Name2, (X1 = Name1; X2 = Name1; X3 = Name1)).
